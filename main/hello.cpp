be9c91c7f81653b4524ac213ff3f64dae1e45887
maverick0304 <nirna.animesh@gmail.com>
2025-03-13 13:00:01 +0530

    2ND

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "PES1UG22AM023\n";
+  cout << "My srn is PES1UG22AM023\n";
   return 0;
 }
